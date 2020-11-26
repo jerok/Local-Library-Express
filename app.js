045295118b47c6fe7df1143afee4b1a77a9a4bf1
Jeremy Kong <j.rlmk@hotmail.com>
2020-11-25 22:41:49 -0800

    Added file changes required for heroku deployment

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" 
 
 // mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://user3625:user3625@cluster0.egkeu.mongodb.net/local_library?retryWrites=true';
+var dev_db_url = 'mongodb+srv://user3625:user3625@cluster0.egkeu.mongodb.net/local_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection Error'));
